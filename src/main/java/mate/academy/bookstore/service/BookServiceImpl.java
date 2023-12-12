13d53204f891bd640ffdaa31de209ac4bebe3203
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-12 19:01:16 +0200

    fix checkstyle

--- src/main/java/mate/academy/bookstore/service/BookServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/BookServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.academy.bookstore.service;
 
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.dto.CreateBookRequestDto;
