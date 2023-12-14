feb9201c1a1624ee7e6dd9d7927f99098b11aa55
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-14 18:09:53 +0200

    fix tag

--- src/main/java/mate/academy/bookstore/controller/BookController.java
+++ src/main/java/mate/academy/bookstore/controller/BookController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Books management", description = "Endpoints for managing books")
+@Tag(name = "Book store management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/books")
