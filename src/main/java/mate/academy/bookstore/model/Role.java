e776e2bebaa093e447aaeb956bcd6e605ef6edaf
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-21 18:49:34 +0200

    add ROLE_ to role.name() in getAuthority() method

--- src/main/java/mate/academy/bookstore/model/Role.java
+++ src/main/java/mate/academy/bookstore/model/Role.java
@@ -26,7 +26,7 @@ public class Role implements GrantedAuthority {
 
     @Override
     public String getAuthority() {
-        return role.name();
+        return "ROLE_" + role.name();
     }
 
     public enum RoleName {
