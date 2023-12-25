1b147494c6d47d8e8ce2ce4a628e36af72b5912e
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-26 01:06:04 +0200

    create AuthenticationService and authenticate() method in it

--- src/main/java/mate/academy/bookstore/dto/user/UserLoginRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/user/UserLoginRequestDto.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class UserLoginRequestDto {
     @Email
     @NotNull(message = "can't be null")
--- src/main/java/mate/academy/bookstore/security/AuthenticationService.java
+++ src/main/java/mate/academy/bookstore/security/AuthenticationService.java
@@ -1,2 +1,23 @@
-package mate.academy.bookstore.security;public class AuthenticationService {
+package mate.academy.bookstore.security;
+
+import lombok.RequiredArgsConstructor;
+import mate.academy.bookstore.dto.user.UserLoginRequestDto;
+import mate.academy.bookstore.dto.user.UserLoginResponseDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(requestDto.getEmail(), requestDto.getPassword()));
+
+        String token = jwtUtil.generateToken(requestDto.getEmail());
+        return new UserLoginResponseDto(token);
+    }
 }
