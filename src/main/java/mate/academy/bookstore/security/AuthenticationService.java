33b0fbc8923855e1a73fd66b00bc5e2afe029415
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-26 00:36:06 +0200

    create JwtUtil class and expiration&secret properties in application.properties

---
+++ src/main/java/mate/academy/bookstore/security/AuthenticationService.java
@@ -0,0 +1,2 @@
+package mate.academy.bookstore.security;public class AuthenticationService {
+}
---
+++ src/main/java/mate/academy/bookstore/security/JwtUtil.java
@@ -0,0 +1,30 @@
+package mate.academy.bookstore.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private Key secret;
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public JwtUtil(@Value("${jwt.secret}") String secretString) {
+        secret = Keys.hmacShaKeyFor(secretString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secret)
+                .compact();
+    }
+}
