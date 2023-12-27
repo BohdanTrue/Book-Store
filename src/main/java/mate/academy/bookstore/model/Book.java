8236e2f7b4ada197b832b7d740c94f3f4556de4b
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-27 19:21:18 +0200

    add categories field in Book model

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -2,12 +2,22 @@ package mate.academy.bookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
 import lombok.Data;
+import org.apache.commons.lang3.builder.EqualsExclude;
+import org.apache.commons.lang3.builder.HashCodeExclude;
+import org.apache.commons.lang3.builder.ToStringExclude;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -32,6 +42,17 @@ public class Book {
 
     @Column(nullable = false)
     private BigDecimal price;
+
+    @ManyToMany
+    @ToStringExclude
+    @EqualsExclude
+    @HashCodeExclude
+    @JoinTable(
+            name = "books_categories",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
 
     private String description;
 
