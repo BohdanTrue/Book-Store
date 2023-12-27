cb33e15ab6812a32e0e89094fb151c0d4836ce34
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-27 21:40:34 +0200

    add Security to CategoryController

--- src/main/java/mate/academy/bookstore/controller/CategoryController.java
+++ src/main/java/mate/academy/bookstore/controller/CategoryController.java
@@ -2,16 +2,21 @@ package mate.academy.bookstore.controller;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.category.CategoryRequestDto;
 import mate.academy.bookstore.dto.category.CategoryResponseDto;
 import mate.academy.bookstore.service.CategoryService;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,27 +25,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     private final CategoryService categoryService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    public CategoryResponseDto createCategory(CategoryRequestDto requestDto) {
+    public CategoryResponseDto createCategory(@Valid @RequestBody CategoryRequestDto requestDto) {
         return categoryService.save(requestDto);
     }
 
+    @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping
     public List<CategoryResponseDto> getAll(Pageable pageable) {
         return categoryService.getAll(pageable);
     }
 
+    @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping("/{id}")
     public CategoryResponseDto getCategoryById(@PathVariable Long id) {
         return categoryService.getById(id);
     }
 
-    @PostMapping
-    public CategoryResponseDto updateCategory(Long id, CategoryRequestDto requestDto) {
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/{id}")
+    public CategoryResponseDto updateCategory(@PathVariable Long id, CategoryRequestDto requestDto) {
         return categoryService.update(id, requestDto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCategory(Long id) {
         categoryService.deleteById(id);
     }
