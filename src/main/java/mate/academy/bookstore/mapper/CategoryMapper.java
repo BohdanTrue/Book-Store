b2683718a06bb62ccabd31d1d7bc5eefc1c56c04
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-27 20:40:46 +0200

    creation CategoryMapper

--- src/main/java/mate/academy/bookstore/mapper/CategoryMapper.java
+++ src/main/java/mate/academy/bookstore/mapper/CategoryMapper.java
@@ -1,2 +1,14 @@
-package mate.academy.bookstore.mapper;public interface CategoryMapper {
+package mate.academy.bookstore.mapper;
+
+import mate.academy.bookstore.config.MapperConfig;
+import mate.academy.bookstore.dto.category.CategoryRequestDto;
+import mate.academy.bookstore.dto.category.CategoryResponseDto;
+import mate.academy.bookstore.model.Category;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface CategoryMapper {
+    CategoryResponseDto toDto(Category category);
+
+    Category toModel(CategoryRequestDto requestDto);
 }
