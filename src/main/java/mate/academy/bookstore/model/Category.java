58650044136ef0e9b864bc8fbf875c655611dd6a
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-27 18:58:04 +0200

    create Category model

---
+++ src/main/java/mate/academy/bookstore/model/Category.java
@@ -0,0 +1,30 @@
+package mate.academy.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Table(name = "categories")
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE categories c SET c.is_deleted = true WHERE c.id=?")
+@Where(clause = "is_deleted = false")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
