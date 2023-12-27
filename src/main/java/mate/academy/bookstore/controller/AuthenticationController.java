ce6d60aef15226247230c16d5ba5a71e706d3e4c
Bohdan Bilko <rapsbodik@gmail.com>
2023-12-27 11:28:51 +0200

    removal JWT implementation

--- src/main/java/mate/academy/bookstore/controller/AuthenticationController.java
+++ src/main/java/mate/academy/bookstore/controller/AuthenticationController.java
@@ -2,12 +2,9 @@ package mate.academy.bookstore.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import mate.academy.bookstore.dto.user.UserLoginRequestDto;
-import mate.academy.bookstore.dto.user.UserLoginResponseDto;
 import mate.academy.bookstore.dto.user.UserRegistrationRequestDto;
 import mate.academy.bookstore.dto.user.UserResponseDto;
 import mate.academy.bookstore.exception.RegistrationException;
-import mate.academy.bookstore.security.AuthenticationService;
 import mate.academy.bookstore.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,16 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserService userService;
-    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
-    }
-
-    @PostMapping("/login")
-    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
-        return authenticationService.authenticate(request);
     }
 }
--- src/main/java/mate/academy/bookstore/dto/user/UserLoginResponseDto.java
+++
@@ -1,4 +0,0 @@
-package mate.academy.bookstore.dto.user;
-
-public record UserLoginResponseDto(String token) {
-}
