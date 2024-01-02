4adc4886f25b61dfabe5ebc9708d26deca9a3ce7
Bohdan Bilko <rapsbodik@gmail.com>
2024-01-02 15:00:13 +0200

    reverting ISBN

--- src/main/java/mate/academy/bookstore/dto/book/BookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/book/BookRequestDto.java
@@ -2,12 +2,12 @@ package mate.academy.bookstore.dto.book;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.Set;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookRequestDto {
@@ -17,7 +17,7 @@ public class BookRequestDto {
     @NotNull(message = "can't be null")
     @Size(min = 3, max = 30)
     private String author;
-    @Pattern(regexp = "^(97(8|9))?\\d{9}(\\d|X)$", message = "Invalid ISBN format")
+    @ISBN
     private String isbn;
     @Positive(message = "must be more than 0")
     @NotNull(message = "can't be null")
