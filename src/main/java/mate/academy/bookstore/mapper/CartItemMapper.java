b42dd641902c829d5916bba891f8d7dd8fdbe61d
Bohdan Bilko <rapsbodik@gmail.com>
2024-01-03 08:49:13 +0200

    create mappers

--- src/main/java/mate/academy/bookstore/mapper/CartItemMapper.java
+++ src/main/java/mate/academy/bookstore/mapper/CartItemMapper.java
@@ -1,2 +1,14 @@
-package mate.academy.bookstore.mapper;public interface CartItemMapper {
+package mate.academy.bookstore.mapper;
+
+import mate.academy.bookstore.config.MapperConfig;
+import mate.academy.bookstore.dto.cart.CartItemResponseDto;
+import mate.academy.bookstore.model.CartItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface CartItemMapper {
+  @Mapping(target = "bookId", source = "book.id")
+  @Mapping(target = "bookTitle", source = "book.title")
+  CartItemResponseDto toDto(CartItem cartItem);
 }
