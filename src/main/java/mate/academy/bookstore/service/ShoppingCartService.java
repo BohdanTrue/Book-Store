438fc6d523cbcea27e87c817a47d0ad819ff0297
Bohdan Bilko <rapsbodik@gmail.com>
2024-01-03 13:45:02 +0200

    create service layer

--- src/main/java/mate/academy/bookstore/service/ShoppingCartService.java
+++ src/main/java/mate/academy/bookstore/service/ShoppingCartService.java
@@ -1,2 +1,15 @@
-package mate.academy.bookstore.service;public interface ShoppingCartService {
+package mate.academy.bookstore.service;
+
+import mate.academy.bookstore.dto.cart.AddQuantityDto;
+import mate.academy.bookstore.dto.cart.CartItemRequestDto;
+import mate.academy.bookstore.dto.cart.ShoppingCartResponseDto;
+
+public interface ShoppingCartService {
+    ShoppingCartResponseDto addBookToShoppingCart(CartItemRequestDto requestDto, Long userId);
+
+    ShoppingCartResponseDto removeBookFromShoppingCart(Long cartItemId, Long userId);
+
+    ShoppingCartResponseDto getShoppingCart(Long userId);
+
+    ShoppingCartResponseDto updateQuantity(Long cartItemId, AddQuantityDto requestDto, Long userId);
 }
