c3b96acf6a5e99094f756aa92c6d49a490bcc8b0
Bohdan Bilko <rapsbodik@gmail.com>
2024-01-08 16:51:06 +0200

    add necessary dependencies for testcontainter

---
+++ src/test/java/mate/academy/bookstore/service/impl/BookServiceImplTest.java
@@ -0,0 +1,12 @@
+package mate.academy.bookstore.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceImplTest {
+
+    @Test
+    void getAll() {
+    }
+}
